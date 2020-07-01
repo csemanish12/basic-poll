8901594d9f11e924743a625df6972f8a74fd0fff
sah_manish <manish.sah@debajyotigroup.com>
2020-07-01 21:02:12 +0545

    create html for views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,31 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template("polls/index.html")
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def details(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You are looking at results of %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(F"You are voting on question {question_id}")
